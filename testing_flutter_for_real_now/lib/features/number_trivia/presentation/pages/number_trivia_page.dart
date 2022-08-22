ffa2d26e2325156d44b27aeaad8504a4cfc64154
Bojacnik <vitek2004@seznam.cz>
2022-08-22 11:32:32 +0200

    "feat: application"

---
+++ testing_flutter_for_real_now/lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -0,0 +1,71 @@
+import 'package:testing_flutter_for_real_now/features/number_trivia/presentation/bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../injection_container.dart';
+
+class NumberTriviaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Number Trivia'),
+      ),
+      body: BlocProvider(
+        builder: (_) => sl<NumberTriviaBloc>(),
+        child: Container(),
+      ),
+    );
+  }
+
+  BlocProvider<NumberTriviaBloc> buildBody(BuildContext context) {
+    return BlocProvider(
+      builder: (_) => sl<NumberTriviaBloc>(),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(
+                height: 10,
+              ),
+              //Top half
+              // ignore: sized_box_for_whitespace
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                child: const Placeholder(),
+              ),
+              const SizedBox(height: 20),
+              //Bottom half
+              Column(
+                children: <Widget>[
+                  const Placeholder(
+                    fallbackHeight: 40,
+                  ),
+                  const SizedBox(height: 10),
+                  // ignore: prefer_const_literals_to_create_immutables
+                  Row(
+                    children: const <Widget>[
+                      // ignore: prefer_const_constructors
+                      Expanded(
+                        child: Placeholder(
+                          fallbackHeight: 30,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: Placeholder(
+                          fallbackHeight: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- testing_flutter_for_real_now/lib/main.dart
+++ testing_flutter_for_real_now/lib/main.dart
@@ -1,6 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'features/number_trivia/presentation/pages/number_trivia_page.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
   await di.init();
-  //runApp(const MyApp());
+  //runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      theme: ThemeData(
+          primaryColor: Colors.green.shade800,
+          // ignore: deprecated_member_use
+          accentColor: Colors.green.shade600),
+      home: NumberTriviaPage(),
+    );
+  }
 }
