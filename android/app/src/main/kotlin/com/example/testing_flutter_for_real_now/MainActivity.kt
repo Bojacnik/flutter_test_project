a05e9c613972d9273f785797c4216c8787f1444c
Bojacnik <vitek2004@seznam.cz>
2022-08-24 14:52:57 +0200

    chore: updated repo using .gitignore

--- android/app/src/main/kotlin/com/example/testing_flutter_for_real_now/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.testing_flutter_for_real_now
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:testing_flutter_for_real_now/core/error/failures.dart';
-import 'package:testing_flutter_for_real_now/features/number_trivia/domain/entities/number_trivia.dart';
-
-abstract class NumberTriviaRepository {
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
-}
